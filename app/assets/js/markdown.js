17d7ea5a1ecf20b2db2051e76011e11d594c60ac
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-08-28 01:03:53 +0530

    (Markdown): buttons added

--- app/assets/js/markdown.js
+++ app/assets/js/markdown.js
@@ -14,12 +14,12 @@ $(function(){
 	        switch (String.fromCharCode(event.which).toLowerCase()) {
 	        case 's':                              	// DETECT <ctrl> + S
 	            event.preventDefault();
-	            alert('ctrl-s');
+	            alert('ctrl + s');
 	            break;
-	        case 'k': 								// DETECT <ctrl> + K
-	            event.preventDefault();
-	            alert('ctrl-f');
-	            break;
+	        // case 'k': 								// DETECT <ctrl> + K
+	        //     event.preventDefault();
+	        //     getSelectionText();
+	        //     break;
 	        // case 'g':
 	        //     event.preventDefault();
 	        //     alert('ctrl-g');
@@ -27,5 +27,35 @@ $(function(){
 	        }
 	    }
 	});
+
+	// function getSelectionText() {
+	//     var text = "";
+	//     if (window.getSelection) {
+	//         text = window.getSelection().toString();
+	//         // window.getSelection().remove();
+	//         console.log("from IF")
+	//     } else if (document.selection && document.selection.type != "Control") {
+	//     	console.log("from ELSE")
+	//         text = document.selection.createRange().text;
+	//     }
+	//     alert(text);
+	// }
+
+	$('.toggle_autosave').click(function(e){
+		e.preventDefault();
+		if($(this).attr('data-status') == 'off')
+		{
+			$(this).find('small').text('Turn OFF auto-save after every 4 mins.');
+			$(this).attr('data-status', 'on');
+			$(this).find('span').removeClass('fa-toggle-off');
+			$(this).find('span').addClass('fa-toggle-on');
+		}
+		else{
+			$(this).find('small').text('Turn ON auto-save after every 4 mins.');
+			$(this).attr('data-status', 'off');
+			$(this).find('span').removeClass('fa-toggle-on');
+			$(this).find('span').addClass('fa-toggle-off');
+		}
+	})
 		
 });
