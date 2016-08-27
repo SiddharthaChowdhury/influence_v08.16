632747a05efaf0404c228fb763a109dfb391f7b9
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-08-27 16:59:32 +0530

    (markdown page): Markdown editor ready and functioanal

--- app/assets/js/markdown.js
+++ app/assets/js/markdown.js
@@ -8,5 +8,24 @@ $(function(){
 	    $('.preview').html(html);
 	});
 
+															
+	$(window).bind('keydown', function(event) {			
+	    if (event.ctrlKey || event.metaKey) {
+	        switch (String.fromCharCode(event.which).toLowerCase()) {
+	        case 's':                              	// DETECT <ctrl> + S
+	            event.preventDefault();
+	            alert('ctrl-s');
+	            break;
+	        case 'k': 								// DETECT <ctrl> + K
+	            event.preventDefault();
+	            alert('ctrl-f');
+	            break;
+	        // case 'g':
+	        //     event.preventDefault();
+	        //     alert('ctrl-g');
+	        //     break;
+	        }
+	    }
+	});
 		
 });
