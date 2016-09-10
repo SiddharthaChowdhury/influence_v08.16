4205601f8d1c85f7e299ee5a50b709c81513c010
Siddhartha Chowdhury <siddhartha.chowdhury@akademe.in>
2016-09-10 23:22:30 +0530

    (Dashboard): Context-menu setup complete. need to make configuration according to our needs

--- app/assets/js/dashboard.js
+++ app/assets/js/dashboard.js
@@ -5,51 +5,13 @@ $(function(){
 
 	$('[data-toggle="tooltip"]').tooltip();
 
-	var _contextMenu = function(){
-		var contextBoxClass= null;
-		var clickedOnClass= null;
-		var closeBtnClass= null;
-		var popupBesideClass= null;
-		this.config = function(obj){
-			this.contextBoxClass = obj.contextBoxClass;
-			this.clickedOnClass = obj.clickedOnClass;
-			this.closeBtnClass = obj.closeBtnClass;
-			this.popupBesideClass = obj.popupBesideClass || obj.clickedOnClass;
-		}
-
-		this.run = function(){
-			var contextMenu 		= $('.'+this.contextBoxClass),
-				popupBesideClass 	= $('.'+this.popupBesideClass),
-				clickedOnClass_str 	= '.'+this.clickedOnClass,
-				closeBtnClass_str 	= '.'+this.closeBtnClass,
-				contextMenu_str		= '.'+this.contextBoxClass; 
-			$('body').click(function(e){  
-				var target = $(e.target); 
-				if(!$(target).is(clickedOnClass_str) && !target.parents(contextMenu_str).length) {
-				    contextMenu.hide();
-				}
-			});
-
-			$(document).on('click', clickedOnClass_str, function(e){
-				e.preventDefault();
-				var coord = popupBesideClass.offset();
-				var width = popupBesideClass.outerWidth()+10;
-				contextMenu.css({top: Math.ceil(coord.top), left: parseInt(Math.ceil(coord.left)+width), position:'absolute'});
-				contextMenu.show();
-			});
-
-			$(document).on('click', closeBtnClass_str, function(){
-				contextMenu.hide();
-			})
-		}
-	}
-	// _contextMenu._contextMenu();
 	var x = new _contextMenu();
 	x.config({
 		contextBoxClass : 'context-menu1',
 		clickedOnClass : 'prespace-context-cog',
 		closeBtnClass : 'close-context-menu',
-		// popupBesideClass : 'progress'
+		// popupBesideClass : 'progress',
+		// disableErrorLog: true
 	})
 	x.run();
 
